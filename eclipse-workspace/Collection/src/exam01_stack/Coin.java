82cdb9ad9490d4cbf1cd810d2d54ca8dcca374b8
kny2383 <kny2383@gmail.com>
2022-01-24 10:43:34 +0900

    1/19 Collcection 실습

---
+++ eclipse-workspace/Collection/src/exam01_stack/Coin.java
@@ -0,0 +1,14 @@
+package exam01_stack;
+
+public class Coin {
+	private int value;
+	
+	public Coin(int value) {
+		this.value = value;
+	}
+	
+	public int getValue() {
+		return value;
+	}
+}
+
---
+++ eclipse-workspace/Collection/src/exam04_comparator/Fruit.java
@@ -0,0 +1,11 @@
+package exam04_comparator;
+
+public class Fruit {
+	public String name;
+	public int price;
+	
+	public Fruit(String name, int price) {
+		this.name = name;
+		this.price = price;
+	}
+}
