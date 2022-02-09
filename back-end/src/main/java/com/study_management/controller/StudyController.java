07bf3ea56ef6c5295ac2b22398ead6acdc315eee
kny2383 <kny2383@gmail.com>
2022-02-09 18:28:53 +0900

    Resolve wrong update problem

--- back-end/src/main/java/com/study_management/controller/StudyController.java
+++
@@ -1,9 +0,0 @@
-package com.study_management.controller;
-
-import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.RequestMapping;
-
-@Controller
-@RequestMapping("/study/*")
-public class StudyController {
-}
