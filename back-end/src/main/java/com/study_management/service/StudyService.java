1bcc0f42d4ca17b1ad3b4726f65596e119be37d5
yu-podong <yu01gun@naver.com>
2022-02-16 15:03:10 +0900

    Add function in service files

--- back-end/src/main/java/com/study_management/service/StudyService.java
+++ back-end/src/main/java/com/study_management/service/StudyService.java
@@ -1,4 +1,31 @@
 package com.study_management.service;
 
+import com.study_management.domain.StudyDTO;
+import com.study_management.domain.StudyMemDTO;
+import com.study_management.domain.StudyRegDTO;
+
+import java.util.List;
+
 public interface StudyService {
+    //
+    public List<StudyDTO> getList();
+
+    //
+    public void create(StudyDTO study);
+
+    //
+    public void apply(StudyRegDTO reg);
+
+    //
+    public StudyDTO mainPage(Integer studyID);
+
+    //
+    public List<StudyMemDTO> member(Integer studyID);
+    public List<StudyRegDTO> regMember(Integer studyID);
+
+    //
+    public void modifyIsReg(Integer regID, StudyMemDTO mem);
+
+    //
+    public void modifyAutho(Integer memID, Integer authoNum);
 }
--- back-end/src/main/java/com/study_management/service/StudyServiceImpl.java
+++ back-end/src/main/java/com/study_management/service/StudyServiceImpl.java
@@ -1,7 +1,60 @@
 package com.study_management.service;
 
+import com.study_management.domain.StudyDTO;
+import com.study_management.domain.StudyMemDTO;
+import com.study_management.domain.StudyRegDTO;
+import com.study_management.mapper.StudyMapper;
+import lombok.AllArgsConstructor;
 import org.springframework.stereotype.Service;
 
+import java.util.List;
+
 @Service
-public class StudyServiceImpl implements StudyService{
+@AllArgsConstructor
+public class StudyServiceImpl implements StudyService {
+    private StudyMapper mapper;
+
+    @Override
+    public List<StudyDTO> getList() {
+        return mapper.getList();
+    }
+
+    @Override
+    public void create(StudyDTO study) {
+        mapper.create(study);
+    }
+
+    @Override
+    public void apply(StudyRegDTO reg) {
+        mapper.insertReg(reg);
+    }
+
+    @Override
+    public StudyDTO mainPage(Integer studyID) {
+        return mapper.get(studyID);
+    }
+
+    @Override
+    public List<StudyMemDTO> member(Integer studyID) {
+        return mapper.getListMember(studyID);
+    }
+
+    @Override
+    public List<StudyRegDTO> regMember(Integer studyID) {
+        return mapper.getListRegMember(studyID);
+    }
+
+    @Override
+    public void modifyIsReg(Integer regID, StudyMemDTO mem) {
+        mapper.deleteReg(regID);
+
+        if(mem.getStudyAutho() > 0) {
+            mapper.createMem(mem);
+        }
+    }
+
+    @Override
+    public void modifyAutho(Integer memID, Integer authoNum) {
+        mapper.updateAutho(memID);
+    }
 }
