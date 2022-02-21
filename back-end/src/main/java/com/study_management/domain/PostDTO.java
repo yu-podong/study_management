5bb792b86d994f08ffdd2d57aa24b64f07976035
kangsu <rkd11011@naver.com>
2022-02-21 09:41:37 +0900

    Update PostDTO.java

--- back-end/src/main/java/com/study_management/domain/PostDTO.java
+++ back-end/src/main/java/com/study_management/domain/PostDTO.java
@@ -1,28 +1,21 @@
 package com.study_management.domain;
 
-import lombok.AllArgsConstructor;
-import lombok.Data;
-import lombok.NoArgsConstructor;
-
 import java.util.Date;
 
-@Data
-@AllArgsConstructor
-@NoArgsConstructor
 public class PostDTO {
-    private Integer postID;
+	private Integer postID;
     private Integer studyID;
     private Integer userID;
     private String title;
-    private Date date;
-    private Integer viewCnt;
-    private Integer comCnt;
     private String content;
 
     // Constructure
 
 
     // Getter
+    public Integer getPostID() {
+        return postID;
+    }
     public Integer getStudyID() {
         return studyID;
     }
@@ -32,23 +25,15 @@ public class PostDTO {
     public Integer getUserID() {
         return userID;
     }
-    public Integer getPostID() {
-        return postID;
-    }
-    public Date getDate() {
-        return date;
-    }
-    public Integer getComCnt() {
-        return comCnt;
-    }
-    public Integer getViewCnt() {
-        return viewCnt;
-    }
+   
     public String getTitle() {
         return title;
     }
 
     // Setter
+    public void setPostID(Integer postID) {
+        this.postID = postID;
+    }
     public void setStudyID(Integer studyID) {
         this.studyID = studyID;
     }
@@ -58,19 +43,9 @@ public class PostDTO {
     public void setUserID(Integer userID) {
         this.userID = userID;
     }
-    public void setPostID(Integer postID) {
-        this.postID = postID;
-    }
-    public void setDate(Date date) {
-        this.date = date;
-    }
-    public void setComCnt(Integer comCnt) {
-        this.comCnt = comCnt;
-    }
+   
     public void setTitle(String title) {
         this.title = title;
     }
-    public void setViewCnt(Integer viewCnt) {
-        this.viewCnt = viewCnt;
-    }
+   
 }
