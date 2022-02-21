54e2390faeb5ee5d25ad359ff602998a7d575a03
kangsu <rkd11011@naver.com>
2022-02-21 09:43:06 +0900

    Update CommuMapper.java

--- back-end/src/main/java/com/study_management/mapper/CommuMapper.java
+++ back-end/src/main/java/com/study_management/mapper/CommuMapper.java
@@ -3,35 +3,52 @@ package com.study_management.mapper;
 import java.util.List;
 
 import com.study_management.domain.CommentDTO;
+import com.study_management.domain.CommentVO;
 import com.study_management.domain.PostDTO;
+import com.study_management.domain.PostListVO;
+import com.study_management.domain.PostVO;
 
 
 
 public interface CommuMapper {
-	//CommentDTO
-	 public List<CommentDTO> commentListGet(int postID);
-	 //댓글 리스트 출력
-	 public void commentInsert(CommentDTO DTO);
-	 //새 댓글 생성 추가
-	 public void commentDelete(int comID);
-	 //댓글 제거
-	 public void commentDeleteAll(int postID);
-	 //질문 삭제
-	 public void commentUpdate(CommentDTO DTO);
-	 //기본 댓글수정
-	 
-	 //PostDTO
-	 public List<PostDTO> postListGet(int studyID);
-	 //질문 리스트 출력
-	 public List<PostDTO> postSelect(int postID);
-	 //질문 선택
-	 public void postInsert(PostDTO DTO);
-	 //질문추가
-	 public void postDelete(int postID);
-	 //질문 삭제
-	 public void postUpdate(PostDTO DTO);//댓글 수정
-	 //질문수정
-	 
+	//CommentDTO===========================================================
+	 public List<PostListVO> PostListVOGet(int studyID);//질문 리스트 보기
+ 
+	 public PostVO PostVOGet(int postID);//질문 보기
 	
-	
+	 public List<CommentVO> CommentVOGet(int postID);//질문 보기
+
+	 
+	 //추가===========================================================
+	 
+	 public void PostDTOInsert(PostDTO DTO);//질문 추가
+	 
+	 public void CommentDTOInsert(CommentDTO DTO);//질문 추가
+	 
+	 //제거===========================================================
+	 public void PostDTODelete(PostDTO postID);//질문 추가
+	 
+	 
+	 public void CommentDTODelete(CommentDTO comID);//질문 추가
+	 
+	 //수정 ===========================================================
+	 public void PostDTOUpdate(PostDTO DTO);//질문 추가
+	 
+	 public void CommentDTOUpdate(CommentDTO DTO);//질문 추가
+	 
+	 //기타===========================================================
+	 
+	 //조회수증가
+	 public void PostViewCntUpdate(int postID);//질문 추가
+	 
+	 //댓글 생성시 댓글 수 증가
+	 public void ComCntUpdate(CommentDTO DTO);//질문 추가
+	 
+	 //댓글 식제시 댓글 수 감수
+	 public void ComCntDecrease(CommentDTO DTO);//질문 추가
+	 
+	 //게시물 삭제시 댓글 삭제
+	 public void AllCommentDelete(PostDTO DTO);//질문 추가
+	 
 }
+
