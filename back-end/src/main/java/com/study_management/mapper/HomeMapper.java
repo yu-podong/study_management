221a56621844f898c128eda163c0646a559feeb5
eonjuseo <ejooss@chungbuk.ac.kr>
2022-02-22 15:33:51 +0900

    Update Home,UserMapper files

--- back-end/src/main/java/com/study_management/mapper/HomeMapper.java
+++ back-end/src/main/java/com/study_management/mapper/HomeMapper.java
@@ -3,8 +3,13 @@ package com.study_management.mapper;
 import com.study_management.domain.UserDTO;
 import org.apache.ibatis.annotations.Mapper;
 
+import javax.servlet.http.HttpSession;
+
 @Mapper
 public interface HomeMapper {
-   // public void insert(UserDTO userDTO);
-   // public void select(UserDTO userDTO);
+
+    public void register(UserDTO userDTO);
+    public void login(UserDTO userDTO, HttpSession httpSession);
+    public void updatePassword(UserDTO userDTO);
+
 }
--- back-end/src/main/java/com/study_management/mapper/UserMapper.java
+++ back-end/src/main/java/com/study_management/mapper/UserMapper.java
@@ -1,4 +1,10 @@
 package com.study_management.mapper;
 
+import com.study_management.domain.UserDTO;
+
 public interface UserMapper {
+
+    public void updateUserInfo(UserDTO userDTO);
+    public void readUserInfo(UserDTO userDTO);
+
 }
