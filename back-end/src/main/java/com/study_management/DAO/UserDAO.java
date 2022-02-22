fed22ba95c74d621c8386cf491918a5ee01eaeaa
eonjuseo <ejooss@chungbuk.ac.kr>
2022-02-22 15:33:04 +0900

    Update UserDAO files

--- back-end/src/main/java/com/study_management/DAO/UserDAO.java
+++ back-end/src/main/java/com/study_management/DAO/UserDAO.java
@@ -7,6 +7,7 @@ import javax.servlet.http.HttpSession;
 
 @Repository
 public interface UserDAO {
+
     // 로그인 처리
     boolean login(UserDTO userDTO, HttpSession httpSession) throws Exception;
 
@@ -14,11 +15,15 @@ public interface UserDAO {
     void register(UserDTO userDTO) throws Exception;
 
     // 회원정보 수정처리
-    void modifyUserInfo(UserDTO userDTO) throws Exception;
-
-    // 회원 비밀번호 수정
-    void modifyPassword(String userID, String password) throws Exception;
+    void updateUserInfo(UserDTO userDTO) throws Exception;
 
     // 회원정보 확인
-    UserDTO viewUserInfo(String userID) throws Exception;
+    UserDTO readUserInfo(Integer userID) throws Exception;
+
+    // 회원 비밀번호 변경
+    void updatePassword(UserDTO userDTO) throws Exception;
+
+    // 아이디 확인
+    UserDAO idCheck(Integer UserID) throws Exception;
+
 }
