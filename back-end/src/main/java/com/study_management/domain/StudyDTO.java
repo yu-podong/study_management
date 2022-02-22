d83568575cd240b4958ee3acad8393c50bab0aa7
yu-podong <yu01gun@naver.com>
2022-02-22 16:54:26 +0900

    Modify StudyDTO

--- back-end/src/main/java/com/study_management/domain/StudyDTO.java
+++ back-end/src/main/java/com/study_management/domain/StudyDTO.java
@@ -3,10 +3,12 @@ package com.study_management.domain;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
+import lombok.ToString;
 
 @Data
 @AllArgsConstructor
 @NoArgsConstructor
+@ToString
 public class StudyDTO {
     private Integer studyID;
     private String studyName;
